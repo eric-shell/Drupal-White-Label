1989100443e135b57e1c4d1d07b4890ceaf7451b
Eric Shell <ericjshell@gmail.com>
2025-10-24 19:06:28 -0700

    Adding DDEV config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,3 +32,16 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+/**
+ * Load local development override configuration, if available.
+ */
+if (file_exists(__DIR__ . '/local.services.yml')) {
+  $settings['container_yamls'][] = __DIR__ . '/local.services.yml';
+}
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
